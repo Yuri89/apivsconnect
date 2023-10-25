85200f12d3d771a3a2961967862ca8cd3360ad6c
Yuri89 <69095138+Yuri89@users.noreply.github.com>
2023-10-25 20:05:30 -0300

    add

--- src/main/java/com/senai/apivsconnect/controllers/UsuarioController.java
+++ src/main/java/com/senai/apivsconnect/controllers/UsuarioController.java
@@ -1,4 +1,25 @@
 package com.senai.apivsconnect.controllers;
 
+import com.senai.apivsconnect.models.UsuarioModel;
+import com.senai.apivsconnect.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/usuarios", produces = {"application/json"})
 public class UsuarioController {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioModel>> listarUsuarios(){
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.findAll());
+    }
 }
--- src/main/java/com/senai/apivsconnect/repositories/UsuarioRepository.java
+++ src/main/java/com/senai/apivsconnect/repositories/UsuarioRepository.java
@@ -3,6 +3,8 @@ package com.senai.apivsconnect.repositories;
 import com.senai.apivsconnect.models.UsuarioModel;
 import org.hibernate.validator.constraints.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<UsuarioModel, UUID> {
 }
