3e55e94b5a560da9494a445e076d168e18414de5
Yuri89 <69095138+Yuri89@users.noreply.github.com>
2023-10-25 21:51:15 -0300

    add

--- src/main/java/com/senai/apivsconnect/controllers/UsuarioController.java
+++ src/main/java/com/senai/apivsconnect/controllers/UsuarioController.java
@@ -1,15 +1,18 @@
 package com.senai.apivsconnect.controllers;
 
+import com.senai.apivsconnect.dtos.UsuarioDto;
 import com.senai.apivsconnect.models.UsuarioModel;
 import com.senai.apivsconnect.repositories.UsuarioRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/usuarios", produces = {"application/json"})
@@ -22,4 +25,29 @@ public class UsuarioController {
     public ResponseEntity<List<UsuarioModel>> listarUsuarios(){
         return ResponseEntity.status(HttpStatus.OK).body(usuarioRepository.findAll());
     }
+
+    @GetMapping("/{idUsuario}")
+    public ResponseEntity<Object> buscarUsuario(@PathVariable(value = "idUsuario") UUID id){
+
+        Optional<UsuarioModel> usuarioBuscado = usuarioRepository.findById(id);
+
+        if (usuarioBuscado.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario não encontrado");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(usuarioBuscado.get());
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> criarUsuario(@RequestBody @Valid UsuarioDto usuarioDto){
+        if (usuarioRepository.findByEmail(usuarioDto.email()) != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email já cadastrado");
+        }
+
+        UsuarioModel novoUsuario = new UsuarioModel();
+        BeanUtils.copyProperties(usuarioDto, novoUsuario);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(novoUsuario));
+    }
+
 }
--- src/main/java/com/senai/apivsconnect/models/UsuarioModel.java
+++ src/main/java/com/senai/apivsconnect/models/UsuarioModel.java
@@ -3,9 +3,10 @@ package com.senai.apivsconnect.models;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.validator.constraints.UUID;
+
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.UUID;
 
 @Getter
 @Setter
